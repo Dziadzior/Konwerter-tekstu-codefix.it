ca5fd8f2a09a1bb9d75a651642be613543bf1fa2
Dziadzior <patrykroszkowski2020@gmail.com>
2024-01-09 20:38:18 +0100

    tat

--- skrypt.js
+++ skrypt.js
@@ -1,9 +1,19 @@
-const piszPole = document.querySelector("textarea"),
-    nazwaPlikuPole = document.querySelector('.nazwa-pliku input'),
-    wybierzMenu = document.querySelector('.zapisz-jako select'),
-    zapiszPrzycisk = document.querySelector('.zapisz-przycisk');
+const piszPole = document.querySelector('textarea'),
+	nazwaPlikuPole = document.querySelector('.nazwa-pliku input'),
+	wybierzMenu = document.querySelector('.zapisz-jako select'),
+	zapiszPrzycisk = document.querySelector('.zapisz-przycisk')
 
-    zapiszPrzycisk.addEventListener('click', () => {
-        const blob = new Blob([piszPole.value], {type: wybierzMenu.value});
-        console.log(blob);
-    })
+wybierzMenu.addEventListener('change', () => {
+	let wybranaOpcja = wybierzMenu.options[wybierzMenu.selectedIndex].text
+	zapiszPrzycisk.innerText = `Zapisz jako ${wybranaOpcja.split(' ')[1]} Plik`
+})
+
+zapiszPrzycisk.addEventListener('click', () => {
+	const blob = new Blob([piszPole.value], { type: wybierzMenu.value })
+	// URL.createObjectURL stworzenie url do objektu
+	const plikURL = URL.createObjectURL(blob)
+	const link = document.createElement('a') // Stworzenie <a> tagu
+	link.download = nazwaPlikuPole.value
+	link.href = plikURL
+	link.click()
+})
